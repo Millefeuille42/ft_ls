48b47e2305ca0787e77190572847d39e1997d064
Millefeuille <mlabouri@student.42.fr>
2023-04-24 13:48:33 +0200

    Fixed flag error management

--- cmd/main.c
+++ cmd/main.c
@@ -459,6 +459,8 @@ char parse_flags(char flags, char *str) {
 				if (str[i2]) {
 					ft_putstr("Invalid flag: ");
 					ft_putchar(str[i2]);
+					ft_putchar('\n');
+					return -1;
 				}
 				break;
 		}
@@ -471,8 +473,13 @@ ls_args parse_args(int argc, char **argv) {
 	ls_args ret = (ls_args){.directories = NULL, .flags = '\0', .err = -1};
 
 	for (int i = 1; i < argc; i++) {
-		if (argv[i][0] == '-') {
+		if (argv[i][0] == '-' && ft_strlen(argv[i]) > 1) {
 			ret.flags = parse_flags(ret.flags, argv[i]);
+			if (ret.flags < 0) {
+				ret.err = -2;
+				delete_list_forward(&directories, safe_free);
+				return ret;
+			}
 			continue;
 		}
 		char *arg = ft_string(argv[i]);
@@ -500,6 +507,8 @@ ls_args parse_args(int argc, char **argv) {
 int main(int argc, char *argv[]) {
 	ls_args args = parse_args(argc, argv);
 	if (args.err) {
+		if (args.err == -2)
+			return 1;
 		panic();
 	}
 
